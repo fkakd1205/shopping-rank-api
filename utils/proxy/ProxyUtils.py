198b899bea4cef0b21ed69df5d14c3dd02cd6dc3
fkakd1205 <fkakd0102@naver.com>
2023-09-05 17:44:14 +0900

    REFACTOR : proxy request url - 환경변수로 뺌

--- utils/proxy/ProxyUtils.py
+++ utils/proxy/ProxyUtils.py
@@ -1,2 +1,7 @@
+import os
+from config.environment.CustomLoadDotEnv import custom_load_dotenv
+
+custom_load_dotenv()
+
 class ProxyUtils():
-    PROXY_REQUEST_URL = "http://kr.smartproxy.com:10000"
+    PROXY_REQUEST_URL = os.environ.get('SMART_PROXY_REQUEST_URL')
